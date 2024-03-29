c48ab6e59af49eb764f9f2aea0c2175ff4db9da1
jyotimishra05 <jyotihra28@gmail.com>
2024-03-29 20:40:56 +0530

    Another approach added

--- Recursion/PhoneNumber.java
+++ Recursion/PhoneNumber.java
@@ -13,21 +13,40 @@ public class PhoneNumber {
     //         findCombination(up.substring(1), p+ch);
     //     }
     // }
-    public static ArrayList<String> findComb(String up, String p){
+    // public static ArrayList<String> findComb(String up, String p){
+    //     if(up.isEmpty()){
+    //         ArrayList<String> list = new ArrayList<>();
+    //         list.add(p);
+    //         // System.out.println(list);
+    //         return list;
+    //     }
+    //     ArrayList<String> list = new ArrayList<>();
+    //     int digit = up.charAt(0)-'0';
+       
+    //         for(int i=(digit-2)*3;i<(digit-1)*3;i++){
+    //         char ch = (char)('a'+i);
+    //         list.addAll(findComb(up.substring(1), p+ch));
+    //     }
+    //     return list;
+    // }
+
+    //Another Approach:
+    static ArrayList<String>findComb(String up , String p){
         if(up.isEmpty()){
-            ArrayList<String> list = new ArrayList<>();
+            ArrayList<String>list=new ArrayList<>();
             list.add(p);
-            // System.out.println(list);
             return list;
         }
-        ArrayList<String> list = new ArrayList<>();
-        int digit = up.charAt(0)-'0';
-       
-            for(int i=(digit-2)*3;i<(digit-1)*3;i++){
-            char ch = (char)('a'+i);
-            list.addAll(findComb(up.substring(1), p+ch));
+        ArrayList<String>outer=new ArrayList<>();
+        String keypad[]={"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+                      // 0    1    2      3      4      5      6       7       8       9  
+        String key= keypad[up.charAt(0)-'0'];
+
+        for(int i=0;i<key.length();i++){
+            outer.addAll(findComb(up.substring(1), p+key.charAt(i)));
+
         }
-        return list;
+        return outer;
     }
     public static ArrayList<String> findCombinationArrayList(String s){
         ArrayList<String>res=findComb(s,"");
@@ -35,7 +54,11 @@ public class PhoneNumber {
     }
     public static void main(String[] args) {
         // findCombination("2","");
-        ArrayList<String>list = findCombinationArrayList("23");
+        String s="23";
+        if(s.isEmpty() || s==null){
+            System.out.println(new ArrayList<>());
+        }
+        ArrayList<String>list = findCombinationArrayList(s);
         System.out.println(list);
     }
 }
